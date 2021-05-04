7d6e1e3dcb155bbfd8b6ec112f75108518557bc0
irossouw2ship <juliasbright@iqsolutions.co.za>
2021-05-04 22:59:07 +0200

    4-model done[4 remains]

---
+++ Julius/Olympia/Olympia.Web.Site/Areas/Admin/Views/Home/ChangePassword.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Identity.Base.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace Olympia.Web.Site.Areas.Admin.Views.Home
+{
+    public class ChangePasswordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
