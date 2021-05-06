5cf2e2e3a29b3c7b1d912c3995287ef8ae06f72e
Ivan Rossouw <irossouw@2ship.com>
2021-05-06 15:41:45 +0200

    Julias Changes2

---
+++ Dreyer/GoldTechInnovation/GoldTechInnovation.Web.Site/Areas/Products/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Grouping.Core.Context.Services;
+using Microsoft.AspNetCore.Mvc;
+using Products.Base.Entities;
+using Products.Core.Context.Services;
+using Products.Core.Controllers;
+
+namespace GoldTechInnovation.Web.Site.Areas.Products.Controllers
+{
+    [Area("Products")]
+    [Route("Products/{controller=home}/{action=index}")]
+    public class HomeController : ProductBaseController
+    {
+        public HomeController(ProductContext service, CategoryContext<Product> categoryService) : base(service, categoryService)
+        {
+        }
+    }
+}
---
+++ Projects/IQTechSolutions/IQTechSolutions.Web/IQTechSolutions.Web.Admin/Areas/Purchases/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InventoryManagement.Core.Context.Configurations.Services;
+using InventoryManagement.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Products.Base.Entities;
+
+namespace IQTechSolutions.Web.Admin.Areas.Purchases.Controllers
+{
+    [Area("Purchases")]
+    [Route("Purchases/{controller=Home}/{action=Index}/{id?}")]
+    public class HomeController : Controller
+    {
+        private GoodReceivedVoucherContext _context;
+
+        public HomeController(GoodReceivedVoucherContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GoodsReceivedVoucher()
+        {
+            var model = new GoodReceivedVoucherAddEditModel
+            {
+                SupplierList = _context.GetAll().ToList()
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult GoodsReceivedVoucher(GoodReceivedVoucherAddEditModel model)
+        {
+            return View();
+        }
+    }
+}
