f63d2fffd1681c9106b0337f5d2ac603bd331006
irossouw2ship <juliasbright@iqsolutions.co.za>
2021-05-08 12:32:55 +0200

    Sat.Commit

--- Julius/Olympia/Olympia.Web.Site/Areas/Employees/Controllers/HomeController.cs
+++ Julius/Olympia/Olympia.Web.Site/Areas/Employees/Controllers/HomeController.cs
@@ -14,5 +14,9 @@ namespace Olympia.Web.Site.Areas.Employees.Controllers
         {
             return View();
         }
+        public IActionResult Office()
+        {
+            return View();
+        }
     }
 }
