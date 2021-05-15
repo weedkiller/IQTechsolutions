5e13b31a2950920bac954e59a165845a168c68a0
irossouw2ship <juliasbright@iqsolutions.co.za>
2021-05-15 16:05:01 +0200

    Controllers-For Modules

---
+++ Julius/CryptoWeb/CryptoWeb/Areas/Prices/Componets/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoWeb.Areas.Prices.Componets
+{
+    public class HomeController : Controller
+    {
+        [Area("Prices")]
+        [Route("Prices/[controller]/[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Julius/CryptoWeb/CryptoWeb/Areas/Shop/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoWeb.Areas.Shop.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Area("Shop")]
+        [Route("Shop/[controller]/[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
