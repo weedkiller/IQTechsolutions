cd96e852926b7d6df5f9706d5cfe4d0a27659629
dreyervorster <dreyer@iqtechsolutions.co.za>
2021-05-19 16:56:23 +0200

    Dreyer_GoldTech_AddTerms_1stCommit
    The LINQ expression could not be translated error in the @await Invoke Call inside Default View inside ProductBanner and ProductWidget.
    
    Added Terms View
    Added About View
    Added Contact View
    
    Added Terms, About and Contact IAction's inside HomeController

--- Modules/Feedback/Feedback.Core/Controllers/TicketBaseController.cs
+++ Modules/Feedback/Feedback.Core/Controllers/TicketBaseController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Feedback.Core.Context.Services;
 using Feedback.Core.Models;
+using GoldTechInnovation.Web.Email;
 using GoogleReCaptcha.V3.Interface;
 using Identity.Base.Entities;
 using Iqt.Base.Enums;
@@ -10,7 +11,6 @@ using Mailing.Base.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Olympia.Web.Email;
 
 // ReSharper disable Mvc.ActionNotResolved
 // ReSharper disable Mvc.ControllerNotResolved
