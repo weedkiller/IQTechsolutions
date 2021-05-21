71c3f2e29a9af66c409ec6e2c75fe6a7b3873a94
dreyervorster <dreyer@iqtechsolutions.co.za>
2021-05-21 14:58:00 +0200

    Dreyer_GoldTech_AddFaq_1stCommit

--- Dreyer/GoldTechInnovation/GoldTechSolutions.Web.Site/Areas/Faqs/Controllers/AnswersController.cs
+++ Dreyer/GoldTechInnovation/GoldTechSolutions.Web.Site/Areas/Faqs/Controllers/AnswersController.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 using Troubleshooting.Core.Context.Services;
 using Troubleshooting.Core.Controllers;
 
-namespace GoldTechSolutions.Web.Site.Areas.FAQs.Controllers
+namespace GoldTechSolutions.Web.Site.Areas.Faqs.Controllers
 {
     [Area("Faqs")]
     [Route("Faqs/[controller]/[action]")]
-    public class HomeController : QuestionBaseController
+    public class AnswersController : AnswerBaseController
     {
         /// <inheritdoc />
         /// <summary>
         /// The default constructor 
         /// </summary>
         /// <param name="service">The injected context service</param>
-        public HomeController(FaqContext service) : base(service) { }
+        public AnswersController(FaqContext service) : base(service) { }
     }
 }
---
+++ Dreyer/GoldTechInnovation/GoldTechSolutions.Web.Site/Areas/Faqs/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using Grouping.Core.Context.Services;
+using Grouping.Core.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Troubleshooting.Base.Entities;
+
+namespace GoldTechSolutions.Web.Site.Areas.Faqs.Controllers
+{
+    [Area("Faqs")]
+    [Route("Faqs/[controller]/[action]")]
+    public class CategoriesController : CategoryBaseController<Question>
+    {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="service">The injected <see cref="CategoryContextService{T}"/> for type <see cref="FaqQuestion"/></param>
+        /// <param name="imageSettings">The injected category image settings</param>
+        /// <param name="iconImageSettings"></param>
+        public CategoriesController(CategoryContext<Question> service) : base(service)
+        {
+        }
+    }
+}
