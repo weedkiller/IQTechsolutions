310acb67567e9fb5d2533a389adb883654d8311a
dreyervorster <dreyer@iqtechsolutions.co.za>
2021-05-21 16:49:14 +0200

    Dreyer_GoldTech_AddBlog_1stCommit

---
+++ Dreyer/GoldTechInnovation/GoldTechSolutions.Web.Site/Areas/Blog/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Blogging.Base.Entities;
+using Blogging.Core.Context.Services;
+using Blogging.Core.Controllers;
+using Grouping.Core.Context.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoldTechSolutions.Web.Site.Areas.Blog.Controllers
+{
+    [Area("Blog")]
+    [Route("Blog/[controller]/[action]")]
+    public class HomeController : BlogBaseController
+    {
+        /// <inheritdoc />
+        /// <summary>
+        /// The default constructor
+        /// </summary>
+        /// <param name="service">The injected context service</param>
+        /// <param name="categoryService">The injected category service</param>
+        public HomeController(BlogContext service, CategoryContext<BlogPost> categoryService)
+            : base(service, categoryService)
+        {
+        }
+    }
+}
